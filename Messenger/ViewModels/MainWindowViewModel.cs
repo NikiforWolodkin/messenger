7eaec5e0787cc6e06d15648c6dbd243cb91a6276
Nick <mrdoggydog37@gmail.com>
2023-10-11 18:58:06 +0300

    Added sidebar chat menu ui

---
+++ Messenger/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Messenger.Models;
+using Messenger.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private ObservableCollection<Chat> _chats;
+        public ObservableCollection<Chat> Chats
+        { 
+            get { return _chats; }
+            set { _chats = value; OnPropertyChanged(); }
+        }
+
+        public MainWindowViewModel()
+        {
+            Chats = new ObservableCollection<Chat> 
+            {
+                new Chat{ LastMessage = "Test message", LastMessageTime = DateTime.Now, Name = "Test user" },
+                new Chat{ LastMessage = "Test message", LastMessageTime = DateTime.Now, Name = "Test user" },
+                new Chat{ LastMessage = "Test message", LastMessageTime = DateTime.Now, Name = "Test user" },
+                new Chat{ LastMessage = "Test message", LastMessageTime = DateTime.Now, Name = "Test user" },
+                new Chat{ LastMessage = "Test message", LastMessageTime = DateTime.Now, Name = "Test user" },
+                new Chat{ LastMessage = "Test message", LastMessageTime = DateTime.Now, Name = "Test user" },
+                new Chat{ LastMessage = "Test message", LastMessageTime = DateTime.Now, Name = "Test user" },
+                new Chat{ LastMessage = "Test message", LastMessageTime = DateTime.Now, Name = "Test user" },
+            };
+        }
+    }
+}
